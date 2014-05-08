f143d04538394927cb46a824516c92c2912ae30f
ashleydavis <ashley@codecapers.com.au>
2014-05-08 21:27:02 +1000

    Fixed an an issue getting a single document from the db.

--- rest/db/collection/get.js
+++ rest/db/collection/get.js
@@ -2,47 +2,50 @@
 // Get a collection or document from the database.
 //
 
-module.exports = {
+module.exports = function () {
+    var mongo = require("mongodb");
 
-	route: ':id?',
+	return {
+		route: ':id?',
 
-	handler: function (req, res, params, done) {
+		handler: function (req, res, params, done) {
 
-		var haveId = req.params.id;
-		var query = haveId ?
-				{ _id: new BSON.ObjectID(req.params.id) } :
-				{};
-		var options = {};
+			var haveId = req.params.id;
+			var query = haveId ?
+					{ _id: new mongo.BSONPure.ObjectID(req.params.id) } :
+					{};
+			var options = {};
 
-		if (haveId) {
-	        params.logger.info("Getting single document: " + req.params.id);
-		}
-		else {
-			params.logger.info("Getting all documents");	
-		}
-
-		params.collection.find(query, options, function(err, cursor) {		
-			if (err) {
-				throw err;
+			if (haveId) {
+		        params.logger.info("Getting single document: " + req.params.id);
+			}
+			else {
+				params.logger.info("Getting all documents");	
 			}
 
-			cursor.toArray(function(err, docs){
-            	if (err) {
-            		throw err;
-            	}
+			params.collection.find(query, options, function(err, cursor) {		
+				if (err) {
+					throw err;
+				}
 
-                if(req.params.id) {
-                    if(docs.length > 0) {
-                        res.json(docs[0]);
-                    } else {
-                        res.send(404);
-                    }
-                } else {
-                    res.json(docs);
-                }
-                
-                done();
-            });						
-		});
-	},
+				cursor.toArray(function(err, docs){
+	            	if (err) {
+	            		throw err;
+	            	}
+
+	                if(req.params.id) {
+	                    if(docs.length > 0) {
+	                        res.json(docs[0]);
+	                    } else {
+	                        res.send(404);
+	                    }
+	                } else {
+	                    res.json(docs);
+	                }
+	                
+	                done();
+	            });						
+			});
+		},
+	};
 };
